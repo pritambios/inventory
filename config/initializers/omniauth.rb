84db45576a53efe534ccd0f27e928f3e5b3cf725
Nabarun Banerjee <nbanerjee@kreeti.com>
2016-11-03 19:25:32 +0530

    fix asset precompile issue

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   secret_keys = Rails.application.secrets
-  provider :google_oauth2,  secret_keys["client_id"],
-                            secret_keys["client_secret"]
+  provider :google_oauth2,  secret_keys["google_client_id"], secret_keys["google_client_secret"]
 end
