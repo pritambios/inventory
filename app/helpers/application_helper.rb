dfbf87b72f7ade21ed6c9f7b9b0661713cb17829
Pritam Pramanik <ppramanik@kreeti.com>
2016-11-09 12:01:30 +0530

    Item show page styling added (#23)
    * Item show page styling added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def back_link
+    link_to ('<i class="fa fa-arrow-left"></i> Back').html_safe, :back
+  end
+
+  def edit_link(path)
+    link_to ('<i class="fa fa-edit" title="Edit Details"></i> Edit').html_safe, path, class: 'btn btn-sm btn-edit'
+  end
+
+  def show_link(path)
+    link_to ('<i class="fa fa-eye"></i>').html_safe, path, class: 'btn-show'
+  end
 end
