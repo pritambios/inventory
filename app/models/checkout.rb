bbb32b757bd22b5d722a5f5816c023c8fd1a18f7
pritam1994 <ppramanik@kreeti.com>
2016-11-11 13:36:56 +0530

    Checking date must be after item purchase date

--- app/models/checkout.rb
+++ app/models/checkout.rb
@@ -4,6 +4,13 @@ class Checkout < ApplicationRecord
 
   validates :checking_out, presence: true
   validates :reason, presence: true
+  validate  :checkout_limitation
 
   scope :order_desending, -> { order('created_at DESC') }
+
+  private
+
+  def checkout_limitation
+    errors.add(:checking_out, "must be after purchase date") unless checking_out > self.item.purchase_on
+  end
 end
