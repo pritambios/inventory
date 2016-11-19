712ef018764580ebeb835aae29ab581be397189f
Nabarun Banerjee <nbanerjee@kreeti.com>
2016-11-20 00:23:44 +0530

    add dynamic event handler

--- app/assets/javascripts/common.js
+++ app/assets/javascripts/common.js
@@ -2,3 +2,8 @@ $(function() {
   $(".alert" ).fadeOut(3000);
   $('.datepicker').datetimepicker({ format: 'DD/MM/YYYY' });
 });
+
+$(document).on('change', "#checkout_item_id", function() {
+  var selected = $(this).find('option:selected');
+  $('#checkout_checkout').data('DateTimePicker').minDate(moment(selected.data('purchase-on'), 'YYYY MM DD'));
+});
