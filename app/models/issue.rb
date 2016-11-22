668a3bffa9161e23637c9d1f3fccc19acc7b17bd
pritam1994 <ppramanik@kreeti.com>
2016-11-22 17:57:17 +0530

    [#95]-User should not select other dates before Item purchase date

--- app/models/issue.rb
+++ app/models/issue.rb
@@ -4,6 +4,11 @@ class Issue < ActiveRecord::Base
   belongs_to :resolution, optional: true
 
   validates :title, presence: true
+  validate  :closed_at_limitation
 
   scope :order_desending, -> { order('created_at DESC') }
+
+  def closed_at_limitation
+    errors.add(:closed_at, "must be after purchase date") unless closed_at > self.item.purchase_on
+  end
 end
