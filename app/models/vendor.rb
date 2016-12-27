53a4255b26e80413f4310f379cf240e70672c481
Apalok-Kreeti <aroy@kreeti.com>
2016-12-27 18:55:30 +0530

    Vendor email issue fixed

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -5,7 +5,7 @@ class Vendor < ActiveRecord::Base
 
   validates :address, :name, presence: true
   validates :mobile, length: { minimum: 10, maximum: 15}, format: { with: VALID_MOBILE_REGEX }, allow_blank: true
-  validates :email, presence: true, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: User::VALID_EMAIL_REGEX }
+  validates :email, length: { maximum: 105 }, uniqueness: { case_sensitive: false }, format: { with: User::VALID_EMAIL_REGEX }, allow_blank: true
 
   scope :order_by_name, -> { order('name') }
 end
