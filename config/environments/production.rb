67eba11b23ae24abba70aedf0de26684de82a190
Nabarun Banerjee <nbanerjee@kreeti.com>
2016-12-29 16:40:15 +0530

    fix production errors

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,9 +87,8 @@ Rails.application.configure do
 
   Rails.application.config.middleware.use ExceptionNotification::Rack,
     email: {
-      deliver_with: :deliver, # Rails >= 4.2.1 do not need this option since it defaults to :deliver_now
       email_prefix: "[Inventory Error] ",
-      sender_address: %{"notifier" <info@kreeti.com>},
+      sender_address: %{"sender" <support@kreeti.com>},
       exception_recipients: %w{ssinghi@kreeti.com nbanerjee@kreeti.com aroy@kreeti.com}
     }
 
