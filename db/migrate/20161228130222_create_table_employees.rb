57a44cf9b45654026c63c625553c9c4655f6e2da
Surendra Singhi <ssinghi@kreeti.com>
2017-01-10 14:46:35 +0530

    comment addition of employee_id column to various tables

--- db/migrate/20161228130222_create_table_employees.rb
+++ db/migrate/20161228130222_create_table_employees.rb
@@ -14,10 +14,10 @@ class CreateTableEmployees < ActiveRecord::Migration
     # remove_column :systems, :employee_id, :integer
     # remove_column :system_histories, :employee_id, :integer
     # remove_column :checkouts, :employee_id, :integer
-    add_reference :items, :employee, index: true, foreign_key: true
-    add_reference :item_histories, :employee, index: true, foreign_key: true
-    add_reference :systems, :employee, index: true, foreign_key: true
-    add_reference :system_histories, :employee, index: true, foreign_key: true
-    add_reference :checkouts, :employee, index: true, foreign_key: true
+    # add_reference :items, :employee, index: true, foreign_key: true
+    # add_reference :item_histories, :employee, index: true, foreign_key: true
+    # add_reference :systems, :employee, index: true, foreign_key: true
+    # add_reference :system_histories, :employee, index: true, foreign_key: true
+    # add_reference :checkouts, :employee, index: true, foreign_key: true
   end
 end
