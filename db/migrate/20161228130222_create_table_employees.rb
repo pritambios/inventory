e594d79f8143a446409459eef5456b569ec3bd7e
Surendra Singhi <ssinghi@kreeti.com>
2017-01-10 14:44:21 +0530

    comment removal of employee_id

--- db/migrate/20161228130222_create_table_employees.rb
+++ db/migrate/20161228130222_create_table_employees.rb
@@ -9,11 +9,11 @@ class CreateTableEmployees < ActiveRecord::Migration
       t.timestamps
     end
 
-    remove_column :items, :employee_id, :integer
-    remove_column :item_histories, :employee_id, :integer
-    remove_column :systems, :employee_id, :integer
-    remove_column :system_histories, :employee_id, :integer
-    remove_column :checkouts, :employee_id, :integer
+    # remove_column :items, :employee_id, :integer
+    # remove_column :item_histories, :employee_id, :integer
+    # remove_column :systems, :employee_id, :integer
+    # remove_column :system_histories, :employee_id, :integer
+    # remove_column :checkouts, :employee_id, :integer
     add_reference :items, :employee, index: true, foreign_key: true
     add_reference :item_histories, :employee, index: true, foreign_key: true
     add_reference :systems, :employee, index: true, foreign_key: true
