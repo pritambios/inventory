674a122533caa6d4164c5d4eb6ce5c2c47be4771
sandeepc19 <schoudhary@gmail.com>
2017-02-09 15:57:38 +0530

    changes made in item parent filter

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
     @items = @items.filter_by_parent(params[:parent]) if params[:parent].present?
     @items = @items.unallocated_items if params[:allocated] == 'false'
     @items = @items.allocated_items if params[:allocated] == 'true'
-    @items = Item.where(id: params[:parent]) if params[:parent].present?
+    @items = @items.where(id: params[:parent]) if params[:parent].present?
     @items = @items.paginate(page: params[:page])
   end
 
