4598b42abb02b61e920a6eb586a581f47a346700
kreetiuser <smahato@kreeti.com>
2018-05-31 12:47:16 +0530

    Implemented rubocop in config directory

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,12 +9,12 @@ Rails.application.configure do
   # Do not eager load code on boot.
   config.eager_load = false
   config.assets.quiet = true
-  config.assets.paths << Rails.root.join("app/assets/images")
+  config.assets.paths << Rails.root.join('app', 'assets', 'images')
   # Show full error reports.
   config.consider_all_requests_local = true
 
   # Enable/disable caching. By default caching is disabled.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
@@ -27,7 +27,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
-  #config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -48,7 +48,7 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-  #config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # RESTfull API config
   config.rest_api_url        = Rails.application.secrets["rest_api_url"]
